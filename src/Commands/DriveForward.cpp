de6f2969c4b13eaa45f5520761c70181a31e646e
SahithKonakalla <sahithk1@gmail.com>
2018-10-13 01:40:34 -0700

    First Version

--- src/Commands/DriveForward.cpp
+++ src/Commands/DriveForward.cpp
@@ -1,23 +1,31 @@
 #include "DriveForward.h"
 
-DriveForward::DriveForward() {
-	// Use Requires() here to declare subsystem dependencies
-	// eg. Requires(Robot::chassis.get());
+DriveForward::DriveForward(double set, double leftPower, double rightPower, int id = 0) {
+	type = id
+	lp = leftPower;
+	rp = rightPower
+	setpoint = set;
+	Requires(drive);
 }
 
 // Called just before this Command runs the first time
 void DriveForward::Initialize() {
-
+	drive->resetEncoders();
 }
 
 // Called repeatedly when this Command is scheduled to run
 void DriveForward::Execute() {
-
+	drive->tankDrive(lp, rp);
+	average = (drive->leftDistance + drive->rightDistance())/2;
+	speed = drive->getSpeed();
+	if (type == 1) {std::cout << "Distance: " + average << std::endl;}
+	if (type == 2) {std::cout << "Velocity: " + speed << std::endl;}
+	if (type == 3) {std::cout << "Velocity: " + speed  + "Motor Output: " + lp + "|" + rp << std::endl;}
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool DriveForward::IsFinished() {
-	return false;
+	return (math.abs(setpoint-average) < 1.5);
 }
 
 // Called once after isFinished returns true
--- src/Commands/DriveForward.h
+++ src/Commands/DriveForward.h
@@ -2,15 +2,21 @@
 #define DriveForward_H
 
 #include "../CommandBase.h"
+#include "math.h"
+#include <iostream>
 
 class DriveForward : public CommandBase {
 public:
-	DriveForward();
+	DriveForward(double set, double leftPower, double rightPower, int id = 0);
 	void Initialize();
 	void Execute();
 	bool IsFinished();
 	void End();
 	void Interrupted();
+private:
+	double setpoint, average, speed;
+	double lp, rp;
+	int type;
 };
 
 #endif  // DriveForward_H
